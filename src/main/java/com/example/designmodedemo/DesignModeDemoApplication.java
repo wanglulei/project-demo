b926c94312f8b7b89b9a4ca2a2d880a17b9dfbda
wanglulei <wanglulei@xmiles.cn>
2021-08-05 15:47:43 +0800

    init

---
+++ src/main/java/com/example/designmodedemo/DesignModeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.designmodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignModeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeDemoApplication.class, args);
+    }
+
+}
